97b1b73914a9d3bcf5a9ae29dd33f8ea7a28b21b
kphudson13 <kphudson@live.ca>
2024-09-23 15:22:55 -0400

    Sept 23 commit 2

--- Kyle_aging_script.R
+++ Kyle_aging_script.R
@@ -2,4 +2,10 @@
 
 # Load packages and data --------------------------------------------------
 
+library(ape)
+library(nlme) #for gls
+library(rotl) #pull from tree of life
+library(tidyverse)
+library(rr2) #for the R2.lik function
+library(gridExtra) #to export the table
 
